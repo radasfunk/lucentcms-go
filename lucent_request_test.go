d8a54f29ae53a1bd785ce617fd55c99bfcad65a8
Aryan Ahmed Anik <thearyanahmed@gmail.com>
2021-07-13 04:44:06 +0600

    added test for protected headers

--- lucent_request_test.go
+++ lucent_request_test.go
@@ -6,14 +6,18 @@ import (
 	"github.com/radasfunk/lucentcmsgo/dev/utils/env"
 )
 
-var client *LucentClient
+var (
+	client        *LucentClient
+	user, channel string
+)
 
 func init() {
 	env.LoadEnv()
 
-	channel := env.Get("LUCENTV3_CHANNEL")
+	channel = env.Get("LUCENTV3_CHANNEL")
+	user = env.Get("LUCENTV3_USER")
+
 	token := env.Get("LUCENTV3_TOKEN")
-	user := env.Get("LUCENTV3_USER")
 
 	client = NewLucentClient(channel, token, user)
 }
@@ -29,11 +33,31 @@ func TestHeadersCanBeAdded(t *testing.T) {
 
 	req.AddHeaders(headers)
 
-	for k,v := range headers {
-		if val, ok := req.Headers[k]; ! ok {
-			t.Errorf("expected %v to be present",k) // empty
+	for k, v := range headers {
+		if val, ok := req.Headers[k]; !ok {
+			t.Errorf("expected %v to be present", k) // empty
 		} else if val != v {
-			t.Errorf("expected %v got %v",v,val) // not empty
-		} 
+			t.Errorf("expected %v got %v", v, val) // not empty
+		}
+	}
+}
+
+func TestProtectedHeadersCanNotBeChanged(t *testing.T) {
+
+	req, _ := client.NewRequest("GET", "documents")
+
+	headers := map[string]string{
+		"Lucent-Channel": "UPDATED_LUCENT_CHANNEL",
+		"Lucent-User":    "UPDATED_RANDOM_USER",
+	}
+
+	req.AddHeaders(headers)
+
+	for k, v := range headers {
+		if val, ok := req.Headers[k]; !ok {
+			t.Errorf("expected %v to be present", k) // empty
+		} else if val == v {
+			t.Errorf("expected %v not to be equal %v", v, val) // not empty
+		}
 	}
 }
