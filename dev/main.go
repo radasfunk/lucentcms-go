5870067312b81073f1247e67ca63945968b4f6fd
Aryan Ahmed Anik <thearyanahmed@gmail.com>
2021-07-13 16:15:21 +0600

    base line request working, wip on response structures

--- dev/main.go
+++ dev/main.go
@@ -23,7 +23,92 @@ type Todo struct {
 	Completed bool   `json:"completed"`
 }
 
+type AResponse struct {
+	Data           []Document
+	Errors, Links  []string
+	Meta, Included map[string]interface{} // TODO add paginator
+}
+
+type Content map[string]interface{}
+
+type Document struct {
+	ID            string      `json:"id"`
+	RequestLocale string      `json:"requestLocale"`
+	Locale        string      `json:"locale"`
+	Schema        string      `json:"schema"`
+	Creator       string      `json:"creator"`
+	Editor        string      `json:"editor"`
+	Status        string      `json:"status"`
+	Version       int         `json:"version"`
+	PublishedAt   time.Time   `json:"publishedAt"`
+	Behind        bool        `json:"behind"`
+	Content       Content     `json:"content"`
+	Subdocs       interface{} `json:"subdocs"`
+	Channel       string      `json:"channel"`
+	UpdatedAt     time.Time   `json:"updatedAt"`
+	CreatedAt     time.Time   `json:"createdAt"`
+}
+
 func main() {
+	channel := env.Get("LUCENTV3_CHANNEL")
+	token := env.Get("LUCENTV3_TOKEN")
+	user := env.Get("LUCENTV3_USER")
+
+	dur := time.Duration(5 * time.Second)
+
+	_ = lucentcmsgo.NewLucentClient(channel, token, user, dur)
+
+	httpClient := http.Client{
+		Timeout: dur,
+	}
+
+	var data = make(map[string]string, 0)
+
+	data["filter[schema]"] = "products"
+	data["include"] = "*"
+
+	requestData, err := json.Marshal(data)
+
+	request, err := http.NewRequest("GET", "https://api.lucentcms.com/api/documents", bytes.NewBuffer(requestData))
+
+	if err != nil {
+		fmt.Printf("error %v", err.Error())
+	}
+
+	request.Header.Add("Accept", "application/json")
+	request.Header.Add("Content-Type", "application/json")
+	request.Header.Add("Lucent-Channel", channel)
+	request.Header.Add("Authorization", "Bearer "+token)
+	request.Header.Add("Accept-Language", "el-GR")
+
+	resp, err := httpClient.Do(request)
+
+	if err != nil {
+		fmt.Printf("error from response %v", err.Error())
+	}
+
+	defer resp.Body.Close()
+
+	body, err := ioutil.ReadAll(resp.Body)
+
+	if err != nil {
+		fmt.Printf("error reading ioutil %v", err.Error())
+	}
+
+	// body = bytes.TrimPrefix(body, []byte("\xef\xbb\xbf")) //
+
+	var response AResponse
+
+	err = json.Unmarshal(body, &response)
+
+	if err != nil {
+		fmt.Printf("error in json response %v", err.Error())
+	}
+
+	fmt.Println(response.Data[0].Channel, "response body")
+}
+
+func aTestRunThatWorked() {
 	channel := env.Get("LUCENTV3_CHANNEL")
 	token := env.Get("LUCENTV3_TOKEN")
 	user := env.Get("LUCENTV3_USER")
@@ -78,61 +163,4 @@ func main() {
 		fmt.Printf("error in json response %v", err.Error())
 	}
 	fmt.Println(response.Id, response.Title, "response body")
-}
-
-func aTestRunThatWorked() {
-	channel := env.Get("LUCENTV3_CHANNEL")
-	token := env.Get("LUCENTV3_TOKEN")
-	user := env.Get("LUCENTV3_USER")
-
-	dur := time.Duration(5 * time.Second)
-
-	_ = lucentcmsgo.NewLucentClient(channel, token, user, dur)
-
-	httpClient := http.Client{
-		Timeout: dur,
-	}
-
-	var data = make(map[string]string, 0)
-
-	data["filter[schema]"] = "products"
-	data["include"] = "*"
-
-	requestData, err := json.Marshal(data)
-
-	request, err := http.NewRequest("GET", "https://api.lucentcms.com/api/documents", bytes.NewBuffer(requestData))
-
-	if err != nil {
-		fmt.Printf("error %v", err.Error())
-	}
-
-	request.Header.Add("Accept", "application/json")
-	request.Header.Add("Lucent-Channel", channel)
-	request.Header.Add("Authorization", "Bearer "+token)
-	request.Header.Add("Accept-Language", "el-GR")
-
-	resp, err := httpClient.Do(request)
-
-	if err != nil {
-		fmt.Printf("error from response %v", err.Error())
-	}
-
-	defer resp.Body.Close()
-
-	body, err := ioutil.ReadAll(resp.Body)
-
-	if err != nil {
-		fmt.Printf("error reading ioutil %v", err.Error())
-	}
-
-	// body = bytes.TrimPrefix(body, []byte("\xef\xbb\xbf")) //
-
-	var response interface{}
-
-	err = json.Unmarshal(body, &response)
-
-	if err != nil {
-		fmt.Printf("error in json response %v", err.Error())
-	}
-	fmt.Println(response, "response body")
 }
